776a167af9d5f3b2874d8f5c026eff0bf369422c
Lindo Khoza <willkhoza@gmail.com>
2020-07-09 22:56:07 +0200

    ???

---
+++ Tutorial 01 - Simple Linear Model/nn_model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import os
+import matplotlib.pyplot as plt
+
+# read data
+data = pd.read_csv("Tutorial 01 - Simple Linear Model/dat1.csv")
+y = data.var10
+X = data.drop("var10", axis=1)
+
+# build model
+out_layer = tf.keras.layers.Dense(units=1, input_shape=[X.shape[1]])
+model = tf.keras.Sequential([out_layer])
+
+# compile model
+model.compile(loss="mse", optimizer=tf.keras.optimizers.Adam(learning_rate=0.1))
+
+# fit model
+history = model.fit(X, y, epochs=150, verbose=True)
+
+plt.plot(history.history["loss"])
+plt.show()
+print("trained layer:", out_layer.weights)
