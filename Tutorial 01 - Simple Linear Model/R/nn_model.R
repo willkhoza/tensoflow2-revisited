d124f787fde9c0301649cc7efb2aefe1e5189900
Lindo Khoza <willkhoza@gmail.com>
2020-07-11 23:00:08 +0200

    R code clinic: remove notes from code

--- Tutorial 01 - Simple Linear Model/R/nn_model.R
+++ Tutorial 01 - Simple Linear Model/R/nn_model.R
@@ -4,15 +4,3 @@ nn.mod <- neuralnet(var10~.,
                     hidden = 0,
                     data = dat) 
 plot(nn.mod)
-ls(nn.mod)
-
-# add a hidden lay
-nn.mod <- neuralnet(var10~., 
-                    hidden = c(2,1),
-                    rep = 150,
-                    data = dat,
-                    threshold = 0.01) 
-
-?neuralnet
-plot(nn.mod)
-ls(nn.mod)
